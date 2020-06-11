40f2a2c444998795f9335c31118be135ec364ad7
mandywl <Mengsi.Wells-Lakeland@vu.edu.au>
2020-06-12 00:52:00 +1000

    Modified master for church app

--- public/js/receipts.js
+++ public/js/receipts.js
@@ -1,32 +1,37 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   onLoad();
 
-  $("#btnInsert").click(function() {
-    $.get("/insertReceipt").then(function() {
+  $("#btnInsert").click(function () {
+    $.get("/insertReceipt").then(function () {
       window.location.replace("/insertReceipt");
     });
   });
 
-  $("#btnExport").click(function() {
-    var content = XLSX.utils.sheet_to_csv(XLSX.utils.table_to_sheet(document.getElementById('receiptTable'), { sheet: "ReceiptExport" }));   
-    saveAs(new Blob([content], { type: "text/plain;charset=utf-8" }), 'export.csv');
+  $("#btnExport").click(function () {
+    var content = XLSX.utils.sheet_to_csv(
+      XLSX.utils.table_to_sheet(document.getElementById("receiptTable"), {
+        sheet: "ReceiptExport",
+      })
+    );
+    saveAs(
+      new Blob([content], { type: "text/plain;charset=utf-8" }),
+      "export.csv"
+    );
   });
 
-  $("#btnLogout").click(function() {
-    $.get("/logout").then(function() {
+  $("#btnLogout").click(function () {
+    $.get("/logout").then(function () {
       window.location.replace("/");
     });
   });
-
 });
 
 function onLoad() {
-  $.get("/api/currentUser", function(data) {
-    $('#welcomeSpan').text(`Welcome ${data.firstName} ${data.lastName}`);
-  });
+  // $.get("/api/currentUser", function(data) {
+  //   $('#welcomeSpan').text(`Welcome ${data.firstName} ${data.lastName}`);
+  // });
 
-  $.get('/api/getReceipts', function(data) {
+  $.get("/api/getReceipts", function (data) {
     renderTable(data);
   });
 }
@@ -36,20 +41,17 @@ async function renderTable(receipts) {
   receiptTable.children().not(":first").remove();
 
   for (let receipt of receipts) {
-    let user = await $.get("/api/getFirstLastName/" + receipt.user)
+    // let user = await $.get("/api/getFirstLastName/" + receipt.user);
 
     const row = $("<tr></tr>");
     const date = new Date(Date.parse(receipt.transactionDate));
 
     const rowCells = $(`
       <td>${date.toDateString()}</td>
-      <td>${receipt.transactionNumber}</td>
       <td>${receipt.transactionType}</td>
-      <td>${receipt.amount}</td>
-      <td>${receipt.tax}</td>
+      <td>${receipt.transactionNumber}</td>
       <td>${receipt.description}</td>
       <td>${receipt.justification}</td>
-      <td>${user.firstName} ${user.lastName}</td>
     `);
 
     receiptTable.append(row.append(rowCells));
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,26 +1,26 @@
 var path = require("path");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    if (req.user) {
-      res.redirect("/receipt");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
-  app.get("/login", function(req, res) {
-    if (req.user) {
-      res.redirect("/receipt");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
-  app.get("/receipt", isAuthenticated, function(req, res) {
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    // if (req.user) {
+    //   res.redirect("/receipt");
+    // }
     res.sendFile(path.join(__dirname, "../public/receipts.html"));
   });
 
-  app.get("/insertReceipt", isAuthenticated, function(req, res) {
+  // app.get("/login", function (req, res) {
+  //   if (req.user) {
+  //     res.redirect("/receipt");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/login.html"));
+  // });
+
+  app.get("/receipt", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/receipts.html"));
+  });
+
+  app.get("/insertReceipt", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/insertReceipt.html"));
   });
 };
